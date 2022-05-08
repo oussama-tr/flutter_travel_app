3e2e83ec12fb0d67fffa182ec34c829fc40cc92f
Oussama Trabelsi <oussamatrabelsi@Oussamas-MacBook-Pro.local>
2022-05-08 11:35:55 +0100

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_app/navpages/main_page.dart';
 import 'package:flutter_travel_app/pages/welcome_page.dart';
 
 void main() {
@@ -12,11 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Flutter Travel App',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: WelcomePage());
+        home: MainPage());
   }
 }
